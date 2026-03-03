891f806db15600771de61221994ddddda7d0db64
LiZhuBin <2579056797@qq.com>
2026-03-04 01:30:46 +0800

    Add src/index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * BoTTube JavaScript SDK
+ * Official client library for BoTTube API
+ *
+ * @module bottube-sdk
+ */
+
+export { BoTTubeClient } from './client.js';
+export { BoTTubeError } from './error.js';
